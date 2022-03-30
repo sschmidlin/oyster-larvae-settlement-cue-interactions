5eb4465c71842cfdca8b0ecb1d8113bccc6b108a
pascalhabluetzel <68325528+pascalhabluetzel@users.noreply.github.com>
2022-03-30 18:02:26 +0200

    calculated response variable

--- settlement_cues.R
+++ settlement_cues.R
@@ -25,14 +25,27 @@ for(i in 1:nrow(data)){
 # Random effect variables: Crab, age, Tray Number, Well
 
 # 1. Calculate response variable from input data
+# Quick fix for the moment: concentrate on data after 30 hours
+# Multiply rows by number of larvae per well for settled and unattached and concatenate the resulting dataframes
+mult_s <- rep(1:nrow(data), data[, 'settled_30hrs'])
+mult_u <- rep(1:nrow(data), data[, 'unattached_30hrs'])
+data_s <- data[mult_s,]
+data_s[, 'settled_30hrs'] <- 1
+data_s <- data_s[, !(names(data_s) %in% c('settled_10hrs', 'unattached_10hrs', 'settled_20hrs', 'unattached_20hrs', 'unattached_30hrs'))]
+data_u <- data[mult_u,]
+data_u[, 'unattached_30hrs'] <- 0
+data_u <- data_u[, !(names(data_u) %in% c('settled_10hrs', 'unattached_10hrs', 'settled_20hrs', 'unattached_20hrs', 'settled_30hrs'))]
+colnames(data_u)[7] <- 'settled_30hrs'
+data <- rbind(data_s, data_u)
 
 # 2. Make a statistical model
 model <- lme(PI_endo ~ sqrt(length) + T_av + NH4_av + NO3_av, random=~1|site, data=data, na.action=na.omit)
-summary(model)
 
 # 3. Does data fulfill model assumptions?
+plot(model)
 
 # 4. Statistical test
+summary(model)
 
 # 5. Visualize model predictions
 
