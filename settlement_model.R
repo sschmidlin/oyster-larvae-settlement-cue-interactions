5e10fb67fc98efba896ad0a522c2f126add7cc60
pascalhabluetzel <pascal.habluetzel@gmail.com>
2023-04-17 23:33:24 +0200

    update

--- settlement_model.R
+++ settlement_model.R
@@ -4,20 +4,20 @@ library(ggplot2)
 A=seq(from = 0, to = 1, by = 0.01)
 # Define a function (linear, exponential, etc.) for the positive cue
 func1 <- function(x) {
-  exp(x)
+  x
 }
 A=sapply(A, func1)
 # Define a correction factor for the positive cue
 k1=1
 
 # Define a vector with all possible values of the positive cue (from 0 to -1)
-#B=rev(seq(from = -1, to = 0, by = 0.01))
-B=seq(from = 0, to = 1, by = 0.01) # Use this line to model two positive cues
+B=seq(from = 0, to = 1, by = 0.01)
 # Define a function (linear, exponential, etc.) for the negative cue
 func2 <- function(x) {
   x
 }
 B=sapply(B, func2)
+summary(B)
 # Define a correction factor for the positive cue
 k2=1
 
@@ -27,7 +27,8 @@ L = matrix(nrow = length(A), ncol = length(B))
 # Run the model
 for (i in 1:length(A)) {
   for (j in 1:length(B)) {
-    L[i,j] = 1 / (1 + exp(-(A[i]*k1 + B[j]*k2)))
+    #L[i,j] = 1 / (1 + exp(-(A[i]*k1 - B[j]*k2))) # first cue positive, second cue negative
+    L[i,j] = 1 / (1 + exp(-(A[i]*k1 + B[j]*k2))) # both cues positive
   }
 }
 
@@ -40,4 +41,5 @@ ggplot(df, aes(x = Var2, y = Var1, fill = value)) +
   scale_fill_gradient(low = "yellow", high = "red") +
   labs(x = "importance of second positive cue", y = "importance of first positive cue", fill="Probability of settlement") +
   coord_fixed() +
+  geom_contour(aes(z=value), colour = "white") +
   theme_minimal()
