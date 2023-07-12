03a5c1a845f038479c551f0c5a6898336b933394
pascalhabluetzel <pascal.habluetzel@gmail.com>
2023-07-12 11:21:09 +0200

    figure 7

--- settlement_model.R
+++ settlement_model.R
@@ -4,6 +4,66 @@ library(ggplot2)
 A=seq(from = 0, to = 1, by = 0.01)
 # Define a function (linear, exponential, etc.) for the positive cue
 func1 <- function(x) {
+  exp(x)
+}
+A=sapply(A, func1)
+# Define a correction factor for the positive cue
+k1=4
+
+# Define a vector with all possible values of the positive cue (from 0 to -1)
+B=seq(from = 0, to = 1, by = 0.01)
+# Define a function (linear, exponential, etc.) for the negative cue
+func2 <- function(x) {
+  x
+}
+B=sapply(B, func2)
+summary(B)
+# Define a correction factor for the positive cue
+k2=10
+
+# Generate a matrix with all possible combinations of positive and negative cue values
+L = matrix(nrow = length(A), ncol = length(B))
+
+# Run the model
+for (i in 1:length(A)) {
+  for (j in 1:length(B)) {
+    L[i,j] = 1 / (1 + exp(-(A[i]*k1 - B[j]*k2))) # first cue positive, second cue negative
+    #L[i,j] = 1 / (1 + exp(-(A[i]*k1 + B[j]*k2))) # both cues positive
+  }
+}
+
+# Change the format of the result matrix for printing
+df <- reshape2::melt(L)
+
+# Print the model result
+ggplot(df, aes(x = Var2, y = Var1, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "darkblue", mid = "lightblue", high = "green", midpoint=0.5) +
+  labs(x = "importance of negative cue", y = "importance of positive cue", fill="Probability of settlement") +
+  coord_fixed() +
+  geom_contour(aes(z=value), colour = "white") +
+  theme_minimal()
+
+
+library(plotly)
+
+plot_ly(df, x = Var2, y = Var1, z = value, type = "surface", 
+        colors = colorRamp(c("darkblue", "lightblue", "green")), 
+        colorbar = list(title = "Probability of settlement"), 
+        contours = list(z = list(show = TRUE, usecolormap = TRUE, highlightcolor = "limegreen", project = list(z = TRUE)))) %>%
+  layout(scene = list(xaxis = list(title = "importance of negative cue"), 
+                      yaxis = list(title = "importance of positive cue"), 
+                      zaxis = list(title = "Value")), 
+         title = "3D Surface Plot with Probability of Settlement")
+
+
+
+library(ggplot2)
+
+# Define a vector with all possible values of the positive cue (from 0 to 1)
+A=seq(from = 0, to = 1, by = 0.01)
+# Define a function (linear, exponential, etc.) for the positive cue
+func1 <- function(x) {
   x
 }
 A=sapply(A, func1)
@@ -21,25 +81,111 @@ summary(B)
 # Define a correction factor for the positive cue
 k2=1
 
+# Define a vector with all possible values of the positive cue (from 0 to -1)
+C=seq(from = 0, to = 1, by = 0.01)
+# Define a function (linear, exponential, etc.) for the negative cue
+func3 <- function(x) {
+  x
+}
+C=sapply(C, func3)
+summary(C)
+# Define a correction factor for the positive cue
+k3=1
+
 # Generate a matrix with all possible combinations of positive and negative cue values
-L = matrix(nrow = length(A), ncol = length(B))
+L <- array(NA, dim = c(length(A), length(B), length(C)))
 
 # Run the model
 for (i in 1:length(A)) {
   for (j in 1:length(B)) {
-    #L[i,j] = 1 / (1 + exp(-(A[i]*k1 - B[j]*k2))) # first cue positive, second cue negative
-    L[i,j] = 1 / (1 + exp(-(A[i]*k1 + B[j]*k2))) # both cues positive
+    for (k in 1:length(C)) {
+    L[i,j,k] = 1 / (1 + exp(-(A[i]*k1 - B[j]*k2 + C[k]*k3))) # first cue positive, second cue negative
+    #L[i,j] = 1 / (1 + exp(-(A[i]*k1 + B[j]*k2))) # both cues positive
+    }
   }
 }
 
 # Change the format of the result matrix for printing
-df <- reshape2::melt(L)
+require(reshape2)
+L.matrix <- aperm(L, c(1, 3, 2))
+L.melted <- melt(L.matrix, varnames = c("A", "C", "B"), value.name = "value")
+
+
+library(plotly)
+library(tidyr)
+
+L.df <- data.frame(expand.grid(A = A, B = B, C = C))
+L.df$value <- as.vector(L)
+
+L.mat <- matrix(L.df$value, nrow = length(B), ncol = length(A), byrow = TRUE)
+
+
+# Create a 3D scatter plot with color-coded points
+fig <- plot_ly(data = L.df, x = ~A, y = ~B, z = ~C, color = ~value,
+               colors = colorRamp(c("blue", "green", "yellow")),
+               marker = list(size = 2)) %>% 
+  add_surface(z = L.mat[, , 1], colors = "gray", opacity = 0.5) %>% # add surface at 0-0-0
+  add_surface(z = L.mat[, , ncol(L.mat)], colors = "gray", opacity = 0.5) # add surface at 1-1-1
+
+# Show the plot
+fig
+
+
+# Create a 3D array L
+A <- c(1, 2, 3)
+B <- c(4, 5, 6)
+C <- c(7, 8, 9)
+
+L <- array(NA, dim = c(length(A), length(B), length(C)))
+
+for (i in 1:length(A)) {
+  for (j in 1:length(B)) {
+    for (k in 1:length(C)) {
+      L[i, j, k] <- A[i] + B[j] + C[k]
+    }
+  }
+}
+
+# Convert L to a long format using tidyr::gather
+L.df <- data.frame(expand.grid(A = A, B = B, C = C))
+L.df$value <- as.vector(L)
+
+# Create a 3D scatter plot with color-coded points
+fig <- plot_ly(data = L.df, x = ~A, y = ~B, z = ~C, color = ~value,
+               colors = colorRamp(c("blue", "green", "yellow")),
+               marker = list(size = 1)) %>%
+  add_markers()
+
+# Show the plot
+fig
+
+
+
+s3d <- scatterplot3d(x = L.melted$A, y = L.melted$B, z = L.melted$C, color = L.melted$value,
+                     main = "3D Scatter Plot", pch = 16, type = "h", lwd = 0.1)
+
+# Add color bar to the plot
+s3d$points3d(x = NA, y = NA, z = NA, col = L.melted$value, pch = 16)
+s3d$legend3d("topright", legend = "Value", col = s3d$color.palette(L.melted$value), pch = 16)
+
+plot(s3d)
+
+plot_ly(L.melted, x = A, y = B, z = value, type = "surface", 
+        colors = colorRamp(c("darkblue", "lightblue", "green")), 
+        colorbar = list(title = "Probability of settlement"), 
+        contours = list(z = list(show = TRUE, usecolormap = TRUE, highlightcolor = "limegreen", project = list(z = TRUE)))) %>%
+  layout(scene = list(xaxis = list(title = "importance of negative cue"), 
+                      yaxis = list(title = "importance of positive cue"), 
+                      zaxis = list(title = "Value")), 
+         title = "3D Surface Plot with Probability of Settlement")
+
 
 # Print the model result
 ggplot(df, aes(x = Var2, y = Var1, fill = value)) +
   geom_tile() +
-  scale_fill_gradient(low = "yellow", high = "red") +
-  labs(x = "importance of second positive cue", y = "importance of first positive cue", fill="Probability of settlement") +
+  scale_fill_gradient2(low = "darkblue", mid = "lightblue", high = "green", midpoint=0.5) +
+  labs(x = "importance of negative cue", y = "importance of positive cue", fill="Probability of settlement") +
   coord_fixed() +
   geom_contour(aes(z=value), colour = "white") +
   theme_minimal()
+
